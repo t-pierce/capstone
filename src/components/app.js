8fabf0136b5c7d7edc2d121111e56d4c07098e1d
tpierce <piercet459@gmail.com>
2021-10-29 21:58:57 -0500

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return ( <
+            div className = 'app' >
+                <h1> Welcome to My Website </h1> 
+            </div>
+        );
+    }
+}
