c56337089f9f7ba3e48276e22b8936bcbdd600e1
tpierce <piercet459@gmail.com>
2021-10-31 23:23:08 -0500

    commit on 'main'

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
 
 import NavigationContainer from './navigation-container';
 import Home from './pages.js/home';
 import Follow from './pages.js/follow';
 import Blog from './pages.js/blog';
 import noMatch from './pages.js/no-match';
-import { query } from 'express';
+
 
 export default class App extends Component {
     constructor() {
         super();
-
-        const isBigScreen = useMediaQuery({ query: '(min-device-width: 1224px)' })
-
-        const isSmallScreen = useMediaQuery({ query: '(max-width: 1224px)' })
 
     }
 
@@ -28,6 +23,7 @@ export default class App extends Component {
                     <div>
                         <NavigationContainer />
 
+
                         <Switch>
                             <Route exact path="/" component={Home} />
                             <Route path="/follow-me" component={Follow} />
@@ -37,13 +33,9 @@ export default class App extends Component {
 
                     </div>
                 </Router>
-
-                <div className="mobile-response">
-                    {isSmallScreen ? small=true  : query }
-                </div>
-
-
+     
             </div>
         );
     }
 }
+
